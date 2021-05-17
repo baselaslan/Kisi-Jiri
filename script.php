74af3a35794e929ff29580a9b091128bd64ca26e
Seline <seline1511@gmail.com>
2021-05-17 19:45:44 +0200

    clean up

--- script.php
+++
@@ -1,9 +0,0 @@
-<?php
-//get files from $_FILES array using variable(s) from namelist
-$wav_file_name = $_FILES["messageName"]["tmp_name"];
-$wav_file_village = $_FILES["messageVillage"]["tmp_name"];
-
-//save to files
-move_uploaded_file($wav_file_name, "Caller_name.wav");
-move_uploaded_file($wav_file_village, "Caller_village.wav");
-?>
